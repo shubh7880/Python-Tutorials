5fa84de3f2ed0c976bf6f6f4b7ef060706a28715
abhijeetsourav <132803890+abhijeetsourav@users.noreply.github.com>
2024-07-30 23:24:34 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,11 +1,8 @@
 print("Hello my friend")
 print("Why we are doing coding in this era of Ai")
-print("*")
-print("**")
-print("***")
-print("****")
-print("*****")
-print("******")
-print("*******")
+for i in range(10):
+  for j in range(i+1):
+    print('*')
+
 print(2**3**3)
 print(2**27)
